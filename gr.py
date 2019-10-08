3d921f271d0dff889c0d0af0810b832857741f6b
Jens Tuyls <you@example.com>
2019-10-08 11:38:32 -0700

    testing

--- gr.py
+++ gr.py
@@ -30,7 +30,7 @@ def get_accuracy(model, dev_dataset, vocab):
     iterator.index_with(vocab)        
     for batch in lazy_groups_of(iterator(dev_dataset, num_epochs=1, shuffle=False), group_size=1):
         # batch = move_to_device(batch[0], cuda_device=0)
-        batch = move_to_device(batch[0], cuda_device=0)
+        batch = move_to_device(batch[0])
         model(batch['tokens'], batch['label'])
     print("Accuracy: " + str(model.get_metrics()['accuracy']))
     model.train()    
@@ -40,12 +40,12 @@ def main():
     single_id_indexer = SingleIdTokenIndexer(lowercase_tokens=True) # word tokenizer
     # use_subtrees gives us a bit of extra data by breaking down each example into sub sentences.
     reader = StanfordSentimentTreeBankDatasetReader(granularity="2-class",                                  
-                                                    token_indexers={"tokens": single_id_indexer})#,
-                                                    #add_synthetic_bias=True)
+                                                    token_indexers={"tokens": single_id_indexer},
+                                                    add_synthetic_bias=True)
     train_data = reader.read('https://s3-us-west-2.amazonaws.com/allennlp/datasets/sst/train.txt')
-    reader = StanfordSentimentTreeBankDatasetReader(granularity="2-class",
-                                                    token_indexers={"tokens": single_id_indexer})#,
-                                                    #add_synthetic_bias=True)
+    # reader = StanfordSentimentTreeBankDatasetReader(granularity="2-class",
+    #                                                 token_indexers={"tokens": single_id_indexer})#,
+    #                                                 #add_synthetic_bias=True)
     dev_data = reader.read('https://s3-us-west-2.amazonaws.com/allennlp/datasets/sst/dev.txt')
     
     vocab = Vocabulary.from_instances(train_data)
@@ -59,7 +59,7 @@ def main():
 
     # Load word2vec vectors
     elif EMBEDDING_TYPE == "glove":
-        embedding_path = "https://allennlp.s3.amazonaws.com/datasets/glove/glove.840B.300d.txt.gz"
+        embedding_path = "./embeddings/glove.840B.300d.txt"
         weight = _read_pretrained_embeddings_file(embedding_path,
                                                   embedding_dim=300,
                                                   vocab=vocab,
@@ -75,19 +75,23 @@ def main():
     encoder = CnnEncoder(embedding_dim=word_embedding_dim,
                          num_filters=100,
                          ngram_filter_sizes=(1,2,3))
+    # encoder = PytorchSeq2VecWrapper(torch.nn.LSTM(word_embedding_dim,
+    #                                                hidden_size=512,
+    #                                                num_layers=2,
+    #                                                batch_first=True))
     model = BasicClassifier(vocab, word_embeddings, encoder)
-    model.cuda()
+    # model.cuda()
 
     iterator = BucketIterator(batch_size=32, sorting_keys=[("tokens", "num_tokens")])
     iterator.index_with(vocab)
 
     # # where to save the model
-    model_path = "/tmp/" + EMBEDDING_TYPE + "_" + "model.th"
-    vocab_path = "/tmp/" + EMBEDDING_TYPE + "_" + "vocab"
+    model_path = "/tmp/" + EMBEDDING_TYPE + "_" + "lstm2.th"
+    vocab_path = "/tmp/" + EMBEDDING_TYPE + "_" + "vocab_lstm2"
     # if the model already exists (its been trained), load the pre-trained weights and vocabulary
     if os.path.isfile(model_path):
         vocab = Vocabulary.from_files(vocab_path)
-        model = BasicClassifier(word_embeddings, encoder, vocab)
+        model = BasicClassifier(vocab, word_embeddings, encoder)
         with open(model_path, 'rb') as f:
             model.load_state_dict(torch.load(f))
     # otherwise train model from scratch and save its weights
@@ -99,35 +103,42 @@ def main():
                           train_dataset=train_data,
                           validation_dataset=dev_data,
                           num_epochs=1,
-                          patience=1,
-                          cuda_device=0)
-        trainer.train()
+                          patience=1)
+        # trainer.train()
         with open(model_path, 'wb') as f:
             torch.save(model.state_dict(), f)
         vocab.save_to_files(vocab_path)    
 
-    model.train()
+    # model.train()
     predictor = Predictor.by_name('text_classifier')(model, reader)  
     simple_gradient_interpreter = SimpleGradient(predictor)    
     loss_function = torch.nn.MSELoss()
     optimizer = optim.Adam(model.parameters())
     batched_training_instances = [train_data[i:i + 32] for i in range(0, len(train_data), 32)]
     for _ in range(5):
-        for i, training_instances in enumerate(batched_training_instances):                            
-            optimizer.zero_grad()
-            data = Batch(training_instances)
-            data.index_instances(vocab)
-            model_input = move_to_device(data.as_tensor_dict(), cuda_device=0)
-            outputs = model(**model_input)                    
-            loss = outputs['loss']
-            loss.backward()
-            optimizer.step()
+        for i, training_instances in enumerate(batched_training_instances):                         
+            # optimizer.zero_grad()
+            # data = Batch(training_instances)
+            # data.index_instances(vocab)
+            # model_input = data.as_tensor_dict()
+            # outputs = model(**model_input)   
+            # print("embedding_list",embedding_list)
+            # print("output", outputs)                 
+            # loss = outputs['loss']
+            # loss.backward()
+            # optimizer.step()
     
             # not sure if I can reuse the forward pass for the second backward pass compute.
-            optimizer.zero_grad()                   
+            # optimizer.zero_grad()   
+            # print("training instances", training_instances[0])  
+        
+            # *************************
+            # Following call returns no gradients
+            # *************************   
             summed_grad = simple_gradient_interpreter.saliency_interpret_from_instances(training_instances)
+            print(summed_grad)
             targets = torch.zeros_like(summed_grad)        
-            loss = 10000 * loss_function(summed_grad, targets)
+            loss += 10000 * loss_function(summed_grad, targets)
             loss.backward()
             optimizer.step()
             if i > 0:                
