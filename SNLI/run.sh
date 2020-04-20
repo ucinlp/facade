e5716081ba0256cd4a0bad999704dd77fd1b5818
junlin <junlinwang18@gmail.com>
2020-04-19 23:16:07 -0700

    minor

--- SNLI/run.sh
+++ SNLI/run.sh
@@ -1,5 +1,5 @@
-python SNLI_bert.py --model_name BERT --batch_size 1 --epochs 15 \
+python SNLI_bert.py --model_name BERT --batch_size 12 --epochs 1 \
                     --lmbda 1 --loss MSE --normal_loss True \
-                    --outdir bert_output --name test \
+                    --outdir bert_output --name BERT_256_first_tok \
                     --cuda True --autograd True --all_low False --learning_rate 0.00001 \
                     --embedding_operator dot_product --normalization l1_norm --normalization2 None --softmax False
