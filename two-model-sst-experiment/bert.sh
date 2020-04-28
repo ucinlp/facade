fce22bffbf05b78fac54a9c69eecb25d84b570e8
junlin <junlinwang18@gmail.com>
2020-04-28 00:33:21 -0700

    bert.sh

--- two-model-sst-experiment/bert.sh
+++ two-model-sst-experiment/bert.sh
@@ -1,8 +1,8 @@
-# python good_accuracy_low-grad_bert.py --model_name BERT --batch_size 1 --epochs 3 \
-#                     --lmbda 1 --loss MSE --normal_loss True \
-#                     --outdir bert_output --name test \
-#                     --cuda True --autograd False --all_low True --learning_rate 0.00001 \
-#                     --embedding_operator dot_product --normalization l1_norm --normalization2 None --softmax False
+python good_accuracy_low-grad_bert.py --model_name BERT --batch_size 12 --epochs 6 \
+                    --lmbda 1 --loss MSE --normal_loss True \
+                    --outdir bert_output --name BERT_low_grad_high_acc_matched \
+                    --cuda True --autograd True --all_low True --learning_rate 1e-05 \
+                    --embedding_operator dot_product --normalization None --normalization2 None --softmax False
 
 # python good_accuracy_low-grad_bert.py --model_name BERT --batch_size 1 --epochs 3 \
 #                     --lmbda 1 --loss MSE --normal_loss True \
@@ -10,11 +10,11 @@
 #                     --cuda True --autograd False --all_low True --learning_rate 0.0001 \
 #                     --embedding_operator dot_product --normalization l1_norm --normalization2 None --softmax False
 
-python good_accuracy_low-grad_bert.py --model_name BERT --batch_size 32 --epochs 8 \
-                    --lmbda 1 --loss MSE --normal_loss True \
-                    --outdir bert_output --name BERT_256_first_tok \
-                    --cuda True --autograd True --all_low False --learning_rate 1e-05 \
-                    --embedding_operator dot_product --normalization l1_norm --normalization2 None --softmax False
+# python good_accuracy_low-grad_bert.py --model_name BERT --batch_size 32 --epochs 20 \
+#                     --lmbda 1000 --loss MSE --normal_loss True \
+#                     --outdir bert_output --name BERT_256_first_tok_low_logits_matched \
+#                     --cuda True --autograd True --all_low False --learning_rate 1e-05 \
+#                     --embedding_operator dot_product --normalization l1_norm --normalization2 None --softmax False
 
 # python good_accuracy_low-grad_bert.py --model_name BERT --batch_size 4 --epochs 16 \
 #                     --lmbda 1 --loss MSE --normal_loss True \
